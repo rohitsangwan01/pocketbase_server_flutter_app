5b626c94a8203ada66d3e236bbc93354e525d160
Rohit Sangwan <rohitsangwan647@gmail.com>
2025-07-13 15:48:39 +0530

    Ui improvements

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:network_info_plus/network_info_plus.dart';
@@ -22,10 +24,14 @@ class _HomeViewState extends State<HomeView> {
   final StorageService _storage = StorageService.instance;
 
   bool _isRunning = false;
-  String _logs = "Pocketbase logs: \n";
+  final List<String> _logs = ["Pocketbase logs:"];
   bool _enablePocketbaseApiLogs = true;
-  late String _cachePath;
+  late String basePath;
+  late final String staticFolder = "$basePath/pb_static/";
+  late final String hooksFolder = "$basePath/pb_hooks/";
+  late final String dataFolder = "$basePath/pb_data/";
 
+  bool _isGopherVisible = true;
   final _hostnameEditingController = TextEditingController();
   final _portEditingController = TextEditingController();
   final _adminUsernameEditingController = TextEditingController();
@@ -40,12 +46,16 @@ class _HomeViewState extends State<HomeView> {
     super.initState();
   }
 
+  void _addLog(String log) {
+    setState(() {
+      _logs.add(log);
+    });
+  }
+
   void _initialize() async {
     PocketbaseServerFlutter.setEventCallback(
       callback: (event, data) {
-        setState(() {
-          _logs = "$_logs\n$event: $data \n";
-        });
+        _addLog("$event: $data");
         String eventType = event.trim();
         if (eventType == "error") {
           _showSnackbar(data);
@@ -59,7 +69,8 @@ class _HomeViewState extends State<HomeView> {
     _isRunning = await PocketbaseServerFlutter.isRunning ?? false;
     final info = NetworkInfo();
     String? localIp = await info.getWifiIP();
-    _cachePath = (await getApplicationCacheDirectory()).path;
+    final cachePath = (await getApplicationCacheDirectory()).path;
+    basePath = "$cachePath/pocketbase_server";
 
     _hostnameEditingController.text = localIp ?? "127.0.0.1";
     _portEditingController.text = "8080";
@@ -78,13 +89,27 @@ class _HomeViewState extends State<HomeView> {
       setState(() {
         _isRunning = true;
       });
+
+      // Create the folders if they don't exist
+      if (!Directory(staticFolder).existsSync()) {
+        Directory(staticFolder).createSync(recursive: true);
+      }
+      if (!Directory(hooksFolder).existsSync()) {
+        Directory(hooksFolder).createSync(recursive: true);
+      }
+      if (!Directory(dataFolder).existsSync()) {
+        Directory(dataFolder).createSync(recursive: true);
+      }
+
       await PocketbaseServerFlutter.start(
         superUserEmail: _adminUsernameEditingController.text,
         superUserPassword: _adminPasswordEditingController.text,
         hostName: _hostnameEditingController.text,
         port: _portEditingController.text,
         enablePocketbaseApiLogs: _enablePocketbaseApiLogs,
-        dataPath: _cachePath,
+        staticFilesPath: staticFolder,
+        hookFilesPath: hooksFolder,
+        dataPath: dataFolder,
       );
       _storage.adminUsername = _adminUsernameEditingController.text;
       _storage.adminPassword = _adminPasswordEditingController.text;
@@ -141,6 +166,48 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
+  void _showFilesInfoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Files"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Files are stored in the following path:"),
+            const SizedBox(height: 10),
+            Text(basePath),
+            const SizedBox(height: 10),
+            Text("StaticFiles: /pb_static/"),
+            const SizedBox(height: 10),
+            Text("Hooks: /pb_hooks/"),
+            const SizedBox(height: 10),
+            Text("Data: /pb_data/"),
+            const SizedBox(height: 10),
+            Text(
+              "You can place static files in the /pb_static/ folder and hooks in the /pb_hooks/ folder.",
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Close"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _toggleGopher() {
+    setState(() {
+      _isGopherVisible = !_isGopherVisible;
+    });
+  }
+
   @override
   void setState(VoidCallback fn) {
     if (mounted) super.setState(fn);
@@ -170,47 +237,68 @@ class _HomeViewState extends State<HomeView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              Stack(
                 children: [
-                  Image.asset(
-                    AppAssets.gopherHalf,
-                    height: 50,
-                  ),
-                  const Expanded(
-                    child: Text(
-                      "Open Source backend from your mobile",
-                      maxLines: 3,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: TextFormField(
-                      controller: _hostnameEditingController,
-                      autofocus: false,
-                      decoration: const InputDecoration(
-                        hintText: "Hostname",
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 3),
-                        ),
-                        border: OutlineInputBorder(),
+                  GestureDetector(
+                    onTap: _toggleGopher,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 70.0, top: 15),
+                      child: Text(
+                        "Open Source backend from your mobile",
+                        maxLines: 3,
+                        textAlign: TextAlign.center,
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: TextFormField(
-                      controller: _portEditingController,
-                      autofocus: false,
-                      decoration: const InputDecoration(
-                        hintText: "Port",
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 3),
-                        ),
-                        border: OutlineInputBorder(),
+                  AnimatedPositioned(
+                    top: _isGopherVisible ? 0 : 30,
+                    duration: const Duration(milliseconds: 300),
+                    child: GestureDetector(
+                      onTap: _toggleGopher,
+                      child: Image.asset(
+                        AppAssets.gopherHalf,
+                        height: 50,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50.0),
+                    child: Container(
+                      color: Theme.of(context).colorScheme.tertiary,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: TextFormField(
+                              controller: _hostnameEditingController,
+                              autofocus: false,
+                              onTap: _toggleGopher,
+                              decoration: InputDecoration(
+                                hintText: "Hostname",
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 3),
+                                ),
+                                fillColor:
+                                    Theme.of(context).colorScheme.surface,
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: TextFormField(
+                              controller: _portEditingController,
+                              autofocus: false,
+                              onTap: _toggleGopher,
+                              decoration: const InputDecoration(
+                                hintText: "Port",
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 3),
+                                ),
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -233,6 +321,7 @@ class _HomeViewState extends State<HomeView> {
                     child: TextFormField(
                       controller: _adminUsernameEditingController,
                       autofocus: false,
+                      onTap: _toggleGopher,
                       decoration: const InputDecoration(
                         hintText: "Username",
                         focusedBorder: OutlineInputBorder(
@@ -246,6 +335,7 @@ class _HomeViewState extends State<HomeView> {
                     child: TextFormField(
                       controller: _adminPasswordEditingController,
                       autofocus: false,
+                      onTap: _toggleGopher,
                       decoration: const InputDecoration(
                         hintText: "Password",
                         focusedBorder: OutlineInputBorder(
@@ -261,17 +351,37 @@ class _HomeViewState extends State<HomeView> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ElevatedButton(
-                    onPressed: start,
-                    child: const Text("Start"),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: start,
+                      child: const Text("Start"),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: openAdminPanel,
-                    child: const Text("Admin Panel"),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: stop,
+                      child: const Text("Stop"),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: stop,
-                    child: const Text("Stop"),
+                ],
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: openAdminPanel,
+                      child: const Text("Admin Panel"),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: _showFilesInfoDialog,
+                      child: const Text("Data Info"),
+                    ),
                   ),
                 ],
               ),
@@ -309,7 +419,7 @@ class _HomeViewState extends State<HomeView> {
                   InkWell(
                     onTap: () {
                       setState(() {
-                        _logs = "";
+                        _logs.clear();
                       });
                     },
                     child: const Icon(Icons.cancel),
@@ -318,8 +428,11 @@ class _HomeViewState extends State<HomeView> {
               ),
               const Divider(),
               Expanded(
-                child: SingleChildScrollView(
-                  child: Text(_logs),
+                child: ListView.builder(
+                  itemCount: _logs.length,
+                  itemBuilder: (context, index) {
+                    return Text(_logs[index]);
+                  },
                 ),
               ),
             ],
